dadfd0e2db0d1f21fe6336bcd9d4a1b05fdeb16a
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-02-01 01:08:53 +0200

    Added general controller

---
+++ src/SimpleWebStore/3. UI/SimpleWebStore.UI/Areas/Abstractions/GeneralController.cs
@@ -0,0 +1,23 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using SimpleWebStore.DAL.UnitOfWork;
+using SimpleWebStore.UI.Areas.Admin.Controllers;
+
+namespace SimpleWebStore.UI.Areas.Abstractions
+{
+    public class GeneralController : Controller
+    {
+        private protected readonly IUnitOfWork _unitOfWork;
+        private protected readonly ILogger<CategoryController> _logger;
+        private protected readonly INotyfService _toastNotification;
+
+        public GeneralController(IUnitOfWork unitOfWork,
+            ILogger<CategoryController> logger,
+            INotyfService toastNotification)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _toastNotification = toastNotification;
+        }
+    }
+}
