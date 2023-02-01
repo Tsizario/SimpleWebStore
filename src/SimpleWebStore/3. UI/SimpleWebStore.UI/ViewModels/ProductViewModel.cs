134f14f025b6a688e5e4c4e3fe048268b7e63429
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-02-01 02:36:29 +0200

    Added ProductViewModel for convient management of related entities

---
+++ src/SimpleWebStore/3. UI/SimpleWebStore.UI/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SimpleWebStore.Domain.Entities;
+
+namespace SimpleWebStore.UI.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+
+        public IEnumerable<SelectListItem> CoverTypeList { get; set; }
+    }
+}
