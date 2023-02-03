ea0a962f95cce54dd469bdb1945d0d6ffde8edc9
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-02-03 02:54:34 +0200

    Added TinyMsiEditor for textarea fields

---
+++ src/SimpleWebStore/3. UI/SimpleWebStore.UI/wwwroot/js/validateInput.js
@@ -0,0 +1,12 @@
+﻿function ValidateInput() {
+    if (document.getElementById("uploadBox").value == "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please upload an image!'
+        })
+        return false;
+    }
+
+    return true;
+}        
