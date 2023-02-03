9e6794194db5f0af77e8316d4a2771fa1b6f6072
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-02-03 06:02:43 +0200

    Updated entities: added AppUser

--- src/SimpleWebStore/1. Domain/SimpleWebStore.Domain/Entities/Product.cs
+++ src/SimpleWebStore/1. Domain/SimpleWebStore.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using SimpleWebStore.Domain.Abstractions;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using SimpleWebStore.Domain.Abstractions;
 using System.ComponentModel.DataAnnotations;
 
 namespace SimpleWebStore.Domain.Entities
@@ -18,28 +19,39 @@ namespace SimpleWebStore.Domain.Entities
 
         [Required]
         [Range(1, 500)]
+        [Display(Name = "List price")]
         public double ListPrice { get; set; }
 
         [Required]
         [Range(1, 500)]
+        [Display(Name = "Price for 1-50+")]
         public double Price { get; set; }
 
         [Required]
+        [Display(Name = "Price for 51-100+")]
         [Range(1, 500)]
         public double Price50 { get; set; }
 
         [Required]
+        [Display(Name = "Price for 100+")]
         [Range(1, 500)]
         public double Price100 { get; set; }
 
+        [ValidateNever]
         public string ImageUrl { get; set; }
 
         [Required]
         public Guid CategoryId { get; set; }
+
+        [ValidateNever]
+        [Display(Name = "Category")]
         public Category Category { get; set; }
 
         [Required]
+        [Display(Name = "Cover Type")]
         public Guid CoverTypeId { get; set; }
+
+        [ValidateNever]
         public CoverType CoverType { get; set; }
     }
 }
---
+++ src/SimpleWebStore/1. Domain/SimpleWebStore.Domain/Entities/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿using SimpleWebStore.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleWebStore.UI.ViewModels
+{
+    public class ShoppingCart
+    {
+        public Product Product { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+        public int Count { get; set; }
+    }
+}
