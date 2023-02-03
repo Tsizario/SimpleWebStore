1f34a842665d99ad13c0407baa1e49b5ea2fb62a
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-02-03 07:34:25 +0200

    Added views and controllers for company

---
+++ src/SimpleWebStore/3. UI/SimpleWebStore.UI/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,118 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using SimpleWebStore.DAL.UnitOfWork;
+using SimpleWebStore.Domain.Constants;
+using SimpleWebStore.Domain.Entities;
+using SimpleWebStore.UI.Areas.Abstractions;
+
+namespace SimpleWebStore.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : GeneralController
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<CompanyController> _logger;
+
+        public CompanyController(IWebHostEnvironment webHostEnvironment,
+            IUnitOfWork unitOfWork,
+            ILogger<CompanyController> logger,
+            INotyfService toastNotification)
+                : base(unitOfWork, toastNotification)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //var allCoverTypes = await _unitOfWork.ProductRepository.GetAllEntitiesAsync();
+
+            //if (allCoverTypes == null)
+            //{
+            //    _toastNotification.Error(Errors.CategorySameNumber);
+            //}
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Upsert(Guid? id)
+        {
+            Company company = new();            
+
+            if (id == null || id == Guid.Empty)
+            {
+                return View(company);
+            }
+            else
+            {
+                company = await _unitOfWork.CompanyRepository.GetEntityAsync(p => p.Id == id);
+
+                return View(company);
+            }
+        }
+
+        // PUT
+        [HttpPost]
+        [ActionName("Upsert")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Upsert(Company company, IFormFile? file)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(company);
+            }
+
+            string notification = null;
+
+            if (company.Id == Guid.Empty)
+            {
+                await _unitOfWork.CompanyRepository.AddEntityAsync(company);
+
+                notification = Notifications.CompanytCreateSuccess;
+            }
+            else
+            {
+                await _unitOfWork.CompanyRepository.UpdateEntityAsync(company);
+
+                notification = Notifications.CompanyUpdateSuccess;
+            }
+
+            await _unitOfWork.SaveAsync();
+
+            _toastNotification.Success(notification);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var deletedItem = await _unitOfWork.CompanyRepository.GetEntityAsync(c => c.Id == id);
+
+            if (deletedItem == null)
+            {
+                return Json(new { success = false, message = Errors.CompanyDeletingError });
+            }
+
+            await _unitOfWork.CompanyRepository.RemoveEntityAsync(id);
+
+            await _unitOfWork.SaveAsync();
+
+            return Json(new { success = true, message = Notifications.CompanyDeleteSuccess });
+        }
+
+        #region API Calls
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var companyList = await _unitOfWork.CompanyRepository.GetAllEntitiesAsync();
+
+            return Json(new { data = companyList });
+        }
+
+        #endregion
+    }
+}
--- src/SimpleWebStore/3. UI/SimpleWebStore.UI/Areas/Customer/Controllers/HomeController.cs
+++ src/SimpleWebStore/3. UI/SimpleWebStore.UI/Areas/Customer/Controllers/HomeController.cs
@@ -1,20 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleWebStore.DAL.UnitOfWork;
+using SimpleWebStore.Domain.Entities;
+using SimpleWebStore.UI.ViewModels;
 
 namespace BookWebStore.UI.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork,
+            ILogger<HomeController> logger)
         {
+            _unitOfWork = unitOfWork;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            return View();
+            IEnumerable<Product> productList = await _unitOfWork.ProductRepository.GetAllEntitiesAsync(
+                includeProps: "Category,CoverType");
+
+            return View(productList);
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = await _unitOfWork.ProductRepository.GetEntityAsync(c => c.Id == id,
+                    includeProps: "Category,CoverType")
+            };
+
+            return View(cartObj);
         }
 
         public IActionResult Privacy()
