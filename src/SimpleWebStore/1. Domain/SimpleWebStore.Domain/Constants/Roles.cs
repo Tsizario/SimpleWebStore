ec14f8e235475dfd45edc4c212bec032e5e11add
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-02-05 01:24:01 +0200

    Added roles to database

---
+++ src/SimpleWebStore/1. Domain/SimpleWebStore.Domain/Constants/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleWebStore.Domain.Constants
+{
+    public static class Roles
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+        public const string Role_User_Comp = "Company";
+        public const string Role_User_Indi = "Individual";
+    }
+}
