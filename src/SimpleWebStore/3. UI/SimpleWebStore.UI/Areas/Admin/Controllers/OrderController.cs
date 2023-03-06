93dc8a999b2f17dab1f790964e53959e77822c7b
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-03-06 22:40:46 +0200

    Added new actions for various statuses.

--- src/SimpleWebStore/3. UI/SimpleWebStore.UI/Areas/Admin/Controllers/OrderController.cs
+++ src/SimpleWebStore/3. UI/SimpleWebStore.UI/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,8 @@ using SimpleWebStore.DAL.UnitOfWork;
 using SimpleWebStore.Domain.Constants;
 using SimpleWebStore.Domain.Entities;
 using SimpleWebStore.UI.Controllers;
+using SimpleWebStore.UI.ViewModels;
+using Stripe;
 using System.Security.Claims;
 
 namespace SimpleWebStore.UI.Areas.Admin.Controllers
@@ -13,6 +15,9 @@ namespace SimpleWebStore.UI.Areas.Admin.Controllers
     [Authorize]
     public class OrderController : GeneralController
     {
+        [BindProperty]
+        public OrderViewModel OrderVM { get; set; }
+
         public OrderController(
             IUnitOfWork unitOfWork, 
             INotyfService toastNotification) 
@@ -23,6 +28,126 @@ namespace SimpleWebStore.UI.Areas.Admin.Controllers
         public async Task<ActionResult> Index()
         {
             return View();
+        }
+
+        public async Task<ActionResult> Details(Guid? orderId)
+        {
+            OrderVM = new OrderViewModel()
+            {
+                OrderHeader = _unitOfWork.OrderHeaderRepository
+                    .GetEntityAsync(u => u.Id == orderId, includeProps: "AppUser").Result,
+                OrderDetails = _unitOfWork.OrderDetailRepository
+                    .GetAllEntitiesAsync(u => u.OrderId == orderId, includeProps: "Product").Result
+            };
+
+            return View(OrderVM);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = Roles.Role_Admin + "," + Roles.Role_Employee)]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> UpdateOrderDetail()
+        {
+            var orderHeaderToUpdate = new OrderHeader()
+            {
+                Id = OrderVM.OrderHeader.Id,
+                Name = OrderVM.OrderHeader.Name,
+                PhoneNumber = OrderVM.OrderHeader.PhoneNumber,
+                Address = OrderVM.OrderHeader.Address,
+                City = OrderVM.OrderHeader.City,
+                PostalCode = OrderVM.OrderHeader.PostalCode
+            };
+
+            if (OrderVM.OrderHeader.Carrier != null)
+            {
+                orderHeaderToUpdate.Carrier = OrderVM.OrderHeader.Carrier;
+            }
+
+            if (OrderVM.OrderHeader.TrackingNumber != null)
+            {
+                orderHeaderToUpdate.Carrier = OrderVM.OrderHeader.TrackingNumber;
+            }
+
+            var orderHeaderFromDb = await _unitOfWork.OrderHeaderRepository.UpdateEntityAsync(orderHeaderToUpdate);
+
+            await _unitOfWork.SaveAsync();
+
+            _toastNotification.Success(Notifications.OrderHeaderUpdateSuccess);
+
+            return RedirectToAction("Details", "Order", new { orderId = orderHeaderFromDb.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = Roles.Role_Admin + "," + Roles.Role_Employee)]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> StartProcessing()
+        {
+            await _unitOfWork.OrderHeaderRepository.UpdateStatus(OrderVM.OrderHeader.Id,
+                Statuses.StatusInProcess);
+
+            await _unitOfWork.SaveAsync();
+
+            _toastNotification.Success(Notifications.OrderStatusUpdateSuccess);
+
+            return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = Roles.Role_Admin + "," + Roles.Role_Employee)]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ShipOrder()
+        {
+            var orderHeaderToUpdate = new OrderHeader()
+            {
+                Id = OrderVM.OrderHeader.Id,
+                TrackingNumber = OrderVM.OrderHeader.TrackingNumber,
+                Carrier = OrderVM.OrderHeader.Carrier,
+                OrderStatus = Statuses.StatusShipped,
+                ShippingDate = DateTime.Now
+            };
+
+            await _unitOfWork.OrderHeaderRepository.UpdateEntityAsync(orderHeaderToUpdate);
+
+            await _unitOfWork.SaveAsync();
+
+            _toastNotification.Success(Notifications.OrderShipSuccess);
+
+            return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = Roles.Role_Admin + "," + Roles.Role_Employee)]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CancelOrder()
+        {
+            var orderFromDb = await _unitOfWork.OrderHeaderRepository.GetEntityAsync(
+                u => u.Id == OrderVM.OrderHeader.Id, tracked: false);
+
+            if (orderFromDb.PaymentStatus == PaymentStatuses.PaymentStatusPending)
+            {
+                var options = new RefundCreateOptions
+                {
+                    Reason = RefundReasons.RequestedByCustomer,
+                    PaymentIntent = orderFromDb.PaymentIntentId
+                };
+
+                var service = new RefundService();
+                Refund refund = service.Create(options);
+
+                await _unitOfWork.OrderHeaderRepository.UpdateStatus(orderFromDb.Id,
+                    Statuses.StatusCancelled, PaymentStatuses.PaymentStatusRefunded);
+            }
+            else
+            {
+                await _unitOfWork.OrderHeaderRepository.UpdateStatus(orderFromDb.Id,
+                    Statuses.StatusCancelled, PaymentStatuses.PaymentStatusCancelled);
+            }
+            
+            await _unitOfWork.SaveAsync();
+
+            _toastNotification.Success(Notifications.OrderCancelSuccess);
+
+            return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
         }
 
         #region API Calls
@@ -47,7 +172,7 @@ namespace SimpleWebStore.UI.Areas.Admin.Controllers
                 case "approved":
                     statusToTheBase = Statuses.StatusApproved;
                     break;
-                default:
+                case "all":
                     statusToTheBase = null;
                     break;
             }
