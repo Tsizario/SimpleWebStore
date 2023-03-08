9e2f87df667368134b3a4daf9964c54772adc878
Tsizario <oleksandr.tsisarenko@nure.ua>
2023-03-08 20:03:00 +0200

    Returned UnitOfWork (because of deleting in last commit by accident)

---
+++ src/SimpleWebStore/2. DAL/SimpleWebStore.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using SimpleWebStore.DAL.Repositories.AppUserRepository;
+using SimpleWebStore.DAL.Repositories.CategoryRepository;
+using SimpleWebStore.DAL.Repositories.CompanyRepository;
+using SimpleWebStore.DAL.Repositories.CoverTypeRepository;
+using SimpleWebStore.DAL.Repositories.OrderHeaderRepository;
+using SimpleWebStore.DAL.Repositories.OrderRepository;
+using SimpleWebStore.DAL.Repositories.ProductRepository;
+using SimpleWebStore.DAL.Repositories.ShoppingCartRepository;
+
+namespace SimpleWebStore.DAL.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository CategoryRepository { get; }
+        ICoverTypeRepository CoverTypeRepository { get; }
+        IProductRepository ProductRepository { get; }
+        ICompanyRepository CompanyRepository { get; }
+        IShoppingCartRepository ShoppingCartRepository { get; }
+        IAppUserRepository AppUserRepository { get; }
+        IOrderHeaderRepository OrderHeaderRepository { get; }
+        IOrderDetailRepository OrderDetailRepository { get; }
+
+        Task<bool> SaveAsync();
+    }
+}
